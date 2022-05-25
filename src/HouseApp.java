f16e2fea22d84fcb4bcfd96ddd8c5de23a1e0b14
junje <2533967790@qq.com>
2022-05-25 17:26:30 +0800

    cesi

--- src/HouseApp.java
+++ src/HouseApp.java
@@ -3,6 +3,6 @@ public class HouseApp {
     public static void main(String[] args) {
         new Houseview().mainMenu();
         System.out.println("==系统已退出==");
-
+        System.out.println("");
     }
 }
